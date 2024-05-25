fd4a43eabf3333c3271bf3ffbc55e0e1e7b016f2
nahoseung <wbc2104@g.hongik.ac.kr>
2024-05-26 01:44:56 +0900

    [Round1] costaurant_project

---
+++ costaurant_project/costaurant/foods/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path('index/',views.index),
+    # path('',views.index),
+    path('menu/',views.index),
+    path('menu/<int:pk>/',views.food_detail),
+    
+]
