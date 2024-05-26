379b327054f075325b6824a55e77f5c51081b21e
ashcircle <jwon_kim@naver.com>
2024-05-26 21:22:28 +0900

    [Round1] Django CRUD - 클래스형 뷰(Class-Based Views)

--- AshCircle/Round1/django-crud/costory/posts/urls.py
+++ AshCircle/Round1/django-crud/costory/posts/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name=''),
-    path('posts/', views.post_list, name='post-list'),
-    path('posts/new/', views.post_create, name='post-create'),
-    path('posts/<int:post_id>/', views.post_detail, name='post-detail'),
-    path('posts/<int:post_id>/edit/', views.post_update, name='post-update'),
-    path('posts/<int:post_id>/delete/', views.post_delete, name='post-delete'),
+    path('', views.IndexRedirectView.as_view(), name=''),
+    path('posts/', views.PostListView.as_view(), name='post-list'),
+    path('posts/new/', views.PostCreateView.as_view(), name='post-create'),
+    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post-update'),
+    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
 ]
--- AshCircle/Round1/django-crud/costory/posts/views.py
+++ AshCircle/Round1/django-crud/costory/posts/views.py
@@ -1,59 +1,44 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import (
+    CreateView, ListView, DetailView, UpdateView, DeleteView, RedirectView
+)
+from django.urls import reverse
 from .models import Post
 from .forms import PostForm
-from django.core.paginator import Paginator
 
 
 # Create your views here.
-def post_list(request):
-    posts = Post.objects.all()
-    paginator = Paginator(posts, 6)
-    curr_page_number = request.GET.get('page')
-    if curr_page_number is None:
-        curr_page_number = 1
-    page = paginator.page(curr_page_number)
-    return render(request, 'posts/post_list.html', {'page': page})
-    context = {'posts': posts}
-    return render(request, 'posts/post_list.html', context)
+class IndexRedirectView(RedirectView):
+    pattern_name = 'post-list'
+
+class PostListView(ListView):
+    model = Post
+    ordering = ['-dt_created']
+    paginate_by = 6
 
 
-def post_detail(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
-    context = {'post': post}
-    return render(request, 'posts/post_detail.html', context)
+class PostDetailView(DetailView):
+    model = Post
 
 
-def post_create(request):
-    if request.method == 'POST':
-        post_form = PostForm(request.POST)
-        if post_form.is_valid():
-            new_post = post_form.save()
-            return redirect('post-detail', post_id=new_post.id)
-    else:
-        post_form = PostForm()
-    return render(request, 'posts/post_form.html', {'form': post_form})
+class PostCreateView(CreateView):
+    model = Post
+    form_class = PostForm
+
+    def get_success_url(self):
+        return reverse('post-detail', kwargs={'pk': self.object.id})
 
 
-def post_update(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
-    if request.method == "POST":
-        post_form = PostForm(request.POST, instance=post)
-        if post_form.is_valid():
-            post_form.save()
-            return redirect('post-detail', post_id=post.id)
-    else:
-        post_form = PostForm(instance=post)
-    return render(request, 'posts/post_form.html', {'form': post_form})
+class PostUpdateView(UpdateView):
+    model = Post
+    form_class = PostForm
+
+    def get_success_url(self):
+        return reverse('post-detail', kwargs={'pk': self.object.id})
 
 
-def post_delete(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
-    if request.method == 'POST':
-        post.delete()
-        return redirect('post-list')
-    else:
-        return render(request, 'posts/post_confirm__delete.html', {'post': post})
+class PostDeleteView(DeleteView):
+    model = Post
+    context_object_name = 'post'
 
-
-def index(request):
-    return redirect('post-list')
+    def get_success_url(self):
+        return reverse('post-list')
