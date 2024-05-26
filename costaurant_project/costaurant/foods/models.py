2b08ad343ac2f17852756ed40be6fd33ea2a288c
Nahoseung <wbc2104@g.hongik.ac.kr>
2024-05-27 01:09:08 +0900

    [Round1] Django 웹개발

--- costaurant_project/costaurant/foods/models.py
+++ costaurant_project/costaurant/foods/models.py
@@ -6,7 +6,7 @@ class menu(models.Model):
     name=models.CharField(max_length=50)
     description=models.CharField(max_length=100)
     price=models.IntegerField()
-    img_source=models.CharField(max_length=260)
+    img_source=models.CharField(max_length=255)
     
     def __str__(self) :
         return self.name
