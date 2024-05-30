094a6080b9a38a4eaf9698c7437cdceba6f35316
arim <arim329@naver.com>
2024-05-31 00:04:02 +0900

    [Round1] 웹개발 시작하기, CRUD (#11)

---
+++ dkfla/Round1/costory/posts/validators.py
@@ -0,0 +1,6 @@
+from django.core.exceptions import ValidationError
+
+def validate_symbols(value):
+  if("@" in value) or ("#" in value) or ('&' in value):
+    raise ValidationError('"@"와 "#"과 "&"은 포함될 수 없습니다.', code='symbol-err')
+  
