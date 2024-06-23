7834aa9d5e75135aa46099ca716450e0ffbfd07d
ehddn6570 <soqlqjs13@naver.com>
2024-06-23 22:01:35 +0900

    [Round2] Django 유저기능 + 모델 파헤치기

---
+++ ehddn6570/Round2/django_coplate2/coplate_project/coplate/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+from .validators import validate_no_special_characters, validate_restaurant_link
+
+
+class User(AbstractUser):
+    nickname = models.CharField(
+        max_length=15, 
+        unique=True, 
+        null=True,
+        validators=[validate_no_special_characters],
+        error_messages={'unique': '이미 사용중인 닉네임입니다.'},
+    )
+
+    profile_pic = models.ImageField(default='default_profile_pic.jpg', upload_to='profile_pics')
+
+    intro = models.CharField(max_length=60, blank=True)
+
+    def __str__(self):
+        return self.email
+
+
+class Review(models.Model):
+    title = models.CharField(max_length=30)
+
+    restaurant_name = models.CharField(max_length=20)
+
+    restaurant_link = models.URLField(validators=[validate_restaurant_link])
+
+    RATING_CHOICES = [
+        (1, '★'),
+        (2, '★★'),
+        (3, '★★★'),
+        (4, '★★★★'),
+        (5, '★★★★★'),
+    ]
+    rating = models.IntegerField(choices=RATING_CHOICES, default=None)
+
+    image1 = models.ImageField(upload_to='review_pics')
+
+    image2 = models.ImageField(upload_to='review_pics', blank=True)
+
+    image3 = models.ImageField(upload_to='review_pics', blank=True)
+
+    content = models.TextField()
+
+    dt_created = models.DateTimeField(auto_now_add=True)
+
+    dt_updated = models.DateTimeField(auto_now=True)
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
---
+++ ehddn6570/Round2/django_coplate2/coplate_project/coplate/views.py
@@ -0,0 +1,149 @@
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.views.generic import (
+    View,
+    ListView, 
+    DetailView, 
+    CreateView, 
+    UpdateView, 
+    DeleteView
+)
+
+from braces.views import LoginRequiredMixin, UserPassesTestMixin
+
+from allauth.account.views import PasswordChangeView
+from allauth.account.models import EmailAddress
+
+from .models import Review, User
+from .forms import ReviewForm, ProfileForm
+from .functions import confirmation_required_redirect
+
+
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        context = {}
+        context['latest_reviews'] = Review.objects.all().order_by('-dt_created')[:4]
+        return render(request, 'coplate/index.html', context)
+
+
+class ReviewListView(ListView):
+    model = Review
+    context_object_name = 'reviews'
+    template_name = 'coplate/review_list.html'
+    paginate_by = 8
+    ordering = ['-dt_created']
+
+
+class ReviewDetailView(DetailView):
+    model = Review
+    template_name = 'coplate/review_detail.html'
+    pk_url_kwarg = 'review_id'
+
+
+class ReviewCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+    model = Review
+    form_class = ReviewForm
+    template_name = 'coplate/review_form.html'
+
+    redirect_unauthenticated_users = True
+    raise_exception = confirmation_required_redirect
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('review-detail', kwargs={'review_id': self.object.id})
+ 
+    def test_func(self, user):
+        return EmailAddress.objects.filter(user=user, verified=True).exists()
+        
+
+class ReviewUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Review
+    form_class = ReviewForm
+    template_name = 'coplate/review_form.html'
+    pk_url_kwarg = 'review_id'
+
+    redirect_unauthenticated_users = False
+    raise_exception = True
+
+    def get_success_url(self):
+        return reverse('review-detail', kwargs={'review_id': self.object.id})
+
+    def test_func(self, user):
+        review = self.get_object()
+        return review.author == user
+
+
+class ReviewDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Review
+    template_name = 'coplate/review_confirm_delete.html'
+    pk_url_kwarg = 'review_id'
+
+    redirect_unauthenticated_users = False
+    raise_exception = True
+
+    def get_success_url(self):
+        return reverse('index') 
+
+    def test_func(self, user):
+        review = self.get_object()
+        return review.author == user
+
+
+class ProfileView(DetailView):
+    model = User
+    template_name = 'coplate/profile.html'
+    pk_url_kwarg = 'user_id'
+    context_object_name = 'profile_user'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user_reviews'] = Review.objects.filter(author__id=self.kwargs.get('user_id')).order_by('-dt_created')[:4]
+        return context
+
+
+class UserReviewListView(ListView):
+    model = Review
+    template_name = 'coplate/user_review_list.html'
+    context_object_name = 'user_reviews'
+    paginate_by = 4
+
+    def get_queryset(self):
+        return Review.objects.filter(author__id=self.kwargs.get('user_id')).order_by('-dt_created')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['profile_user'] = get_object_or_404(User, id=self.kwargs.get('user_id'))
+        return context
+
+
+class ProfileSetView(LoginRequiredMixin, UpdateView):
+    model = User
+    form_class = ProfileForm
+    template_name = 'coplate/profile_set_form.html'
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+    def get_success_url(self):
+        return reverse('index')
+
+
+class ProfileUpdateView(LoginRequiredMixin, UpdateView):
+    model = User
+    form_class = ProfileForm
+    template_name = 'coplate/profile_update_form.html'
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+    def get_success_url(self):
+        return reverse('profile', kwargs={'user_id': self.request.user.id})
+
+
+class CustomPasswordChangeView(LoginRequiredMixin, PasswordChangeView):
+    def get_success_url(self):
+        return reverse('profile', kwargs={'user_id': self.request.user.id})
+        
