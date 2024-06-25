4f534905914d4580a5e4d4d040007b93b4c62f6e
mirupio <162397816+mirupio@users.noreply.github.com>
2024-06-25 16:49:50 +0900

    Delete Round2/coplate_project directory

--- Round2/coplate_project/coplate/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CoplateConfig(AppConfig):
-    name = 'coplate'
--- Round2/coplate_project/coplate/functions.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import redirect
-
-from allauth.account.utils import send_email_confirmation
-
-
-def confirmation_required_redirect(self, request):
-    send_email_confirmation(request, request.user)
-    return redirect('account_email_confirmation_required')
