2c5be94c0c91ec39c7e709f176b39be01139937e
SHIM KYUMIN <129478908+GalaxyDimension@users.noreply.github.com>
2024-06-30 14:48:34 +0900

    Galaxy dimension (#31)
    * [Round1] Django 웹 개발 시작하기&CRUD
    
    * [Round1] Django 웹 개발 시작하기&CRUD
    
    * [Round2] Django 유저 기능 구현하기 & 모델 파헤치기
    
    ---------
    
    Co-authored-by: GalaxyDimension <cyan52863@gamil.com>

---
+++ GalaxyDimension/Round2/Django 모델 파헤치기/coplate_project/coplate/admin.py
@@ -0,0 +1,42 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.contenttypes.admin import GenericStackedInline
+from .models import User, Review, Comment, Like
+
+
+
+class CommentInline(admin.StackedInline):
+    model = Comment
+
+
+class LikeInline(GenericStackedInline):
+    model = Like
+    
+
+class UserInline(admin.StackedInline):
+    model = User.following.through
+    fk_name = 'to_user'
+    verbose_name = 'Follower'
+    verbose_name_plural = 'Follower'
+
+UserAdmin.fieldsets += ('Custom fields', {'fields': ('nickname', 'profile_pic', 'intro', 'following',)}),
+UserAdmin.inlines = (UserInline,)
+
+class ReviewAdmin(admin.ModelAdmin):
+    inlines = (
+        CommentInline,
+        LikeInline,
+    )
+
+
+class CommentAdmin(admin.ModelAdmin):
+    inlines = (
+        LikeInline,
+    )
+admin.site.register(User, UserAdmin)
+
+admin.site.register(Review, ReviewAdmin)
+
+admin.site.register(Comment, CommentAdmin)
+
+admin.site.register(Like)
---
+++ GalaxyDimension/Round2/Django 모델 파헤치기/coplate_project/coplate/models.py
@@ -0,0 +1,105 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+
+from .validators import validate_no_special_characters, validate_restaurant_link
+
+
+class User(AbstractUser):
+    nickname = models.CharField(
+        max_length=15, 
+        unique=True, 
+        null=True,
+        validators=[validate_no_special_characters],
+        error_messages={'unique': '이미 사용중인 닉네임입니다.'},
+    )
+
+    profile_pic = models.ImageField(default='default_profile_pic.jpg', upload_to='profile_pics')
+
+    intro = models.CharField(max_length=60, blank=True)
+    
+    following = models.ManyToManyField(
+        'self', 
+        symmetrical=False, 
+        blank=True, 
+        related_name='followers'
+    )
+    
+    email_domain = models.CharField(max_length=30, null=True)
+
+    def __str__(self):
+        return self.email
+
+
+class Review(models.Model):
+    title = models.CharField(max_length=30)
+
+    restaurant_name = models.CharField(max_length=20)
+
+    restaurant_link = models.URLField(validators=[validate_restaurant_link])
+
+    RATING_CHOICES = [
+        (1, '★'),
+        (2, '★★'),
+        (3, '★★★'),
+        (4, '★★★★'),
+        (5, '★★★★★'),
+    ]
+    rating = models.IntegerField(choices=RATING_CHOICES, default=None)
+
+    image1 = models.ImageField(upload_to='review_pics')
+
+    image2 = models.ImageField(upload_to='review_pics', blank=True)
+
+    image3 = models.ImageField(upload_to='review_pics', blank=True)
+
+    content = models.TextField()
+
+    dt_created = models.DateTimeField(auto_now_add=True)
+
+    dt_updated = models.DateTimeField(auto_now=True)
+    
+    likes = GenericRelation('Like')
+    
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews')
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['-dt_created']
+        
+class Comment(models.Model):
+    content = models.CharField(max_length=500, blank=False)
+    
+    dt_created = models.DateTimeField(auto_now_add=True)
+    
+    dt_updated = models.DateTimeField(auto_now=True)
+    
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    
+    review = models.ForeignKey(Review, on_delete=models.CASCADE, related_name='comments')
+    
+    likes = GenericRelation('Like')
+    
+    def __str__(self):
+        return self.content[:30]
+    
+    class Meta:
+        ordering = ['-dt_created']
+
+class Like(models.Model):
+    dt_created = models.DateTimeField(auto_now_add=True)
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='likes')
+    
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    
+    object_id = models.PositiveIntegerField()
+    
+    liked_object = GenericForeignKey('content_type', 'object_id')
+    
+    def __str__(self):
+        return f"({self.user}, {self.liked_object})"
+    
