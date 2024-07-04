5b3965271b91536e9f25ca2781017fb0013cbde0
Hoseung <135733369+Nahoseung@users.noreply.github.com>
2024-07-05 00:10:52 +0900

    Round2 Django Model (#30)
    * [Round1] costaurant_project
    
    * Django 웹개발/CRUD
    
    * [Round1] Django 웹개발
    
    * [Round1] Django CRUD
    
    * Django[Round1] 파일 디렉토리 수정
    
    * [Round1] 실습과제 제출 파일구조 수정
    
    * Round2 django model
    
    * Round2 django_model

---
+++ Nahoseung/Round1/Django_web/costaurant/foods/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class menu(models.Model):
+    name=models.CharField(max_length=50)
+    description=models.CharField(max_length=100)
+    price=models.IntegerField()
+    img_source=models.CharField(max_length=255)
+    
+    def __str__(self) :
+        return self.name
+    
